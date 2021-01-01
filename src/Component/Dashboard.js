e93ca8e94d5de209c63e63f38569acf92f742a71
vipula_jivani <v1bhalodia@gmail.com>
2021-01-01 17:49:30 +0530

    set dashboard ui with click and alert

--- src/Component/Dashboard.js
+++ src/Component/Dashboard.js
@@ -1,26 +1,59 @@
-import React, {Component} from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Button, Alert, StyleSheet} from 'react-native';
+import Slider from '@react-native-community/slider';
 
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const SliderView = (props) => {
+  const {title, value} = props;
+  const [sliderValue, setSliderValue] = useState(value);
+  return (
+    <View>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.count}>Value {sliderValue}</Text>
+      <Slider
+        maximumValue={100}
+        minimumValue={0}
+        minimumTrackTintColor="#307ecc"
+        maximumTrackTintColor="#000000"
+        step={1}
+        value={sliderValue}
+        onValueChange={(value) => setSliderValue(value)}
+        style={styles.slider}
+      />
+      <Button
+        title="Button"
+        onPress={() => Alert.alert(`Slider Value is ${sliderValue}`)}
+      />
+    </View>
+  );
+};
 
+class Dashboard extends React.Component {
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <Text>Dashboard</Text>
-      </SafeAreaView>
+      <View style={styles.container}>
+        <SliderView title={'Slider 1'} value={0} />
+        <SliderView title={'Slider 2'} value={0} />
+        <SliderView title={'Slider 3'} value={0} />
+      </View>
     );
   }
 }
+
 export default Dashboard;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  title: {
+    fontSize: 20,
   },
+  count: {
+    color: '#ababab',
+    marginTop: 10,
+  },
+  button: {
+    width: 100,
+  },
+  container: {
+    margin: 10,
+  },
+  slider: {height: 50},
 });
