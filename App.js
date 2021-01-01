72af92def1f1f72f8b262c4b3319e97e278d49a9
vipula_jivani <v1bhalodia@gmail.com>
2021-01-01 15:24:07 +0530

    create login page with ui

--- App.js
+++ App.js
@@ -6,14 +6,60 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput,
+  Button,
 } from 'react-native';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phone: '',
+      code: '+91',
+    };
+  }
+  onSubmit() {
+    console.log('onsubmit');
+  }
   render() {
     return (
-      <SafeAreaView>
-        <Text>app</Text>
+      <SafeAreaView style={styles.container}>
+        <Text>Mobile Number</Text>
+        <View style={styles.innerContainer}>
+          <TextInput
+            style={styles.inputBox}
+            onChangeText={(text) => onChangeText(text)}
+            value={this.state.code}
+          />
+          <TextInput
+            style={styles.inputBox}
+            placeholder={'Enter mobile numer'}
+            onChangeText={(text) => this.setState({phone: text})}
+            value={this.state.phone}
+            keyboardType="phone-pad"
+            maxLength={10}
+          />
+        </View>
+        <Button title="submit" onPress={() => this.onSubmit()} />
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    margin: 10,
+  },
+  inputBox: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    fontSize: 14,
+  },
+});
