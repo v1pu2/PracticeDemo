be3904bd6b4d3493eeaa2bf8465e3f9998511d42
vipula_jivani <v1bhalodia@gmail.com>
2021-01-01 14:59:10 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>app</Text>
+      </SafeAreaView>
+    );
+  }
+}
